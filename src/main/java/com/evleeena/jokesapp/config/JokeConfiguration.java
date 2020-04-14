9c74eb9fc562a087f659ef1e4baceffb7822fb51
Elena Zaets <evleeena@gmail.com>
2020-04-14 18:06:48 +0300

    added joke config

---
+++ src/main/java/com/evleeena/jokesapp/config/JokeConfiguration.java
@@ -0,0 +1,14 @@
+package com.evleeena.jokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- src/main/java/com/evleeena/jokesapp/services/ChuckNorrisJokeService.java
+++ src/main/java/com/evleeena/jokesapp/services/ChuckNorrisJokeService.java
@@ -3,10 +3,16 @@ package com.evleeena.jokesapp.services;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class ChuckNorrisJokeService implements JokeService {
+
+    @Resource
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
     @Override
     public String showJoke() {
-        return new ChuckNorrisQuotes().getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
